7da5be938d538bf83b5026b85d94ddbc3607e43b
halit-windows <halitislam.icli@saglik.gov.tr>
2025-12-31 16:23:20 +0300

    initial two project

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,20 +1,21 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
     public int expectedMinutesInOven() {
         return 40;
     }
 
     // TODO: define the 'remainingMinutesInOven()' method
-    public int remainingMinutesInOven() {
-        return expextedMinutesInOven() - 30;
+    public int remainingMinutesInOven(int remaining) {
+        int expected = expectedMinutesInOven();
+        return expected - remaining;
     }
 
     // TODO: define the 'preparationTimeInMinutes()' method
-    public int preparationTimeInMinutes() {
-        
+    public int preparationTimeInMinutes(int lasagnaCount) {
+        return lasagnaCount * 2;
     }
     // TODO: define the 'totalTimeInMinutes()' method
-    public int totalTimeInMinutes() {
-        
+    public int totalTimeInMinutes(int lasagnaCount, int minute) {
+        int preparationTime = preparationTimeInMinutes(lasagnaCount);
+        return preparationTime + minute;
     }
 }
