2b292e36a7322d827a5e7d640f7703fbcb1a3717
halit-windows <halitislam.icli@saglik.gov.tr>
2025-12-31 15:24:37 +0300

    Inıtıal commit with Java Lasanga from Exercism

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,20 @@
+public class Lasagna {
+    // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven() {
+        return expextedMinutesInOven() - 30;
+    }
+
+    // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes() {
+        
+    }
+    // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes() {
+        
+    }
+}
